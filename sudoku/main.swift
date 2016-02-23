e9ea3081635bc02952acdd78c92f6c37ee8c7c58
WATANABE Yuki <yuki.watanabe@access-company.com>
2016-02-23 15:25:14 +0900

    Initial Commit

---
+++ sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sudoku
+//
+//  Created by Watanabe Yuki on 2016/02/23.
+//  Copyright © 2016 ACCESS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
