9f6661468a65e519f98df8a7bc414c6fcb4b3580
WATANABE Yuki <yuki.watanabe@access-company.com>
2016-02-23 15:29:31 +0900

    Add Sudoku Library project

---
+++ SudokuLibrary/SudokuLibrary/SudokuLibrary.h
@@ -0,0 +1,19 @@
+//
+//  SudokuLibrary.h
+//  SudokuLibrary
+//
+//  Created by Watanabe Yuki on 2016/02/23.
+//  Copyright © 2016 ACCESS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SudokuLibrary.
+FOUNDATION_EXPORT double SudokuLibraryVersionNumber;
+
+//! Project version string for SudokuLibrary.
+FOUNDATION_EXPORT const unsigned char SudokuLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SudokuLibrary/PublicHeader.h>
+
+
